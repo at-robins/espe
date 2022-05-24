95b3232b784708f1be690c10b6fefb4603dd293a
at-robins <30956028+at-robins@users.noreply.github.com>
2022-05-24 09:29:56 +0200

    [0] Frontend project cleanup
     The frontend project was cleaned up and optimised.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,21 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "@quasar/extras/material-icons/material-icons.css";
+import "quasar/src/css/index.sass";
 
-const app = createApp(App)
+import { Quasar } from "quasar";
+import quasarLang from "quasar/lang/en-GB";
+import quasarIconSet from "quasar/icon-set/svg-material-icons";
 
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(router);
+
+app.use(Quasar, {
+  plugins: {},
+  lang: quasarLang,
+  iconSet: quasarIconSet,
+});
+
+app.mount("#app");
