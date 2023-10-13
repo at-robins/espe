f6ae00bf1deb1192615a8b63a9fc2ebca335be86
at-robins <30956028+at-robins@users.noreply.github.com>
2023-10-13 07:34:53 +0200

    [17] Added db retries during init
    A retry mechanism for creating the initial database connection pool
    has been added.

--- backend/src/application/database.rs
+++ backend/src/application/database.rs
@@ -1,3 +1,5 @@
+use std::{thread::sleep, time::Duration};
+
 use actix_web::web;
 use diesel::{
     connection::SimpleConnection,
@@ -10,13 +12,27 @@ use super::{
     error::{SeqError, SeqErrorType, DEFAULT_INTERNAL_SERVER_ERROR_EXTERNAL_MESSAGE},
 };
 
+/// The base frequency of retries in milliseconds.
+const BASE_RETRY_FREQUENCY: u64 = 10;
+/// The maximum number of retries for establishing a database connection.
+const MAX_RETRIES: u32 = 6;
+
 #[derive(Debug)]
 pub struct CustomConnection {}
 
 impl diesel::r2d2::CustomizeConnection<SqliteConnection, diesel::r2d2::Error> for CustomConnection {
     fn on_acquire(&self, conn: &mut SqliteConnection) -> Result<(), diesel::r2d2::Error> {
         (|| {
-            conn.batch_execute("PRAGMA journal_mode = WAL; PRAGMA synchronous = NORMAL; PRAGMA foreign_keys = ON; PRAGMA busy_timeout = 10000;")?;
+            for n_retry in 0..MAX_RETRIES {
+                let connection_result = conn.batch_execute("PRAGMA journal_mode = WAL; PRAGMA synchronous = NORMAL; PRAGMA foreign_keys = ON; PRAGMA busy_timeout = 10000;");
+                if connection_result.is_err() && n_retry < MAX_RETRIES {
+                    // If there was an error, retry with an exponentially increasing timeout.
+                    sleep(Duration::from_millis(BASE_RETRY_FREQUENCY * 2_u64.pow(n_retry)));
+                } else {
+                    // Return the result upon successful connection or when the maximum number of retries is reached.
+                    return connection_result;
+                }
+            };
             Ok(())
         })()
         .map_err(diesel::r2d2::Error::QueryError)
