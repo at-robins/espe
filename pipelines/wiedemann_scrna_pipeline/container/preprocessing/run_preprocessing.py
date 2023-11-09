cf2c6edb0a67bd5337ff5bf2fb1f85fdbbb0d983
Robin <robin.schenk@tum.de>
2023-11-09 16:27:01 +0100

    [22] Preprocessing multiple files
    Preprocessing multiple files has been implemented.

--- pipelines/wiedemann_scrna_pipeline/container/preprocessing/run_preprocessing.py
+++ pipelines/wiedemann_scrna_pipeline/container/preprocessing/run_preprocessing.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python
 """This module runs data preprocessing."""
 
+import csv
+import json
 import numpy as np
+import os
 import scanpy as sc
 import seaborn as sns
 from scipy.stats import median_abs_deviation
 
-import json
-import math
-import multiprocessing
-import os
-import sys
-
 MOUNT_PATHS = json.loads(os.environ.get("MOUNT_PATHS"))
-INPUT_FOLDER = MOUNT_PATHS["input"]
+INPUT_FOLDER = MOUNT_PATHS["input"] + "/"
 
 # Setup of scanpy.
 sc.settings.verbosity = 2
@@ -24,6 +21,7 @@ sc.settings.set_figure_params(
 )
 sc.settings.figdir = MOUNT_PATHS["output"]
 
+
 def is_outlier(data, metric: str, n_mad: int):
     """
     Detects outliers according to the defined metric by
@@ -33,20 +31,21 @@ def is_outlier(data, metric: str, n_mad: int):
     Keyword arguments:
     data -- the dataset
     metric -- the metric to use for outlier detection
-    n_mad -- a datapoint is defined as outlier if its distance from 
-    the metric median is more than n_mad times the 
+    n_mad -- a datapoint is defined as outlier if its distance from
+    the metric median is more than n_mad times the
     median absolute deviation (MAD)
     """
     m = data.obs[metric]
     return np.abs(m - np.median(m)) > n_mad * median_abs_deviation(m)
 
-def process_data(file_path):
+
+def process_data(file_path, output_folder_path, metrics_writer):
     """
-    TODO
+    Processes datasets and removes low qulaity reads.
     """
     print(f"Processing file {file_path}")
     print("\tReading data...")
-    adata = sc.read_10x_h5(filename = file_path)
+    adata = sc.read_10x_h5(filename=file_path)
     print("\tMaking variable names unique...")
     adata.var_names_make_unique()
 
@@ -63,35 +62,33 @@ def process_data(file_path):
     )
 
     print("\tGenerating QC plots...")
-    sns.displot(
-        data = adata.obs["total_counts"],
-        bins=100,
-        kde=False
-    ).set(
-        xlabel ="Total counts per cell",
-        ylabel = "Number of cells"
-    ).savefig(f"{MOUNT_PATHS['output']}/histo_raw_total_counts.svg")
-    sc.pl.violin(
-        adata = adata,
-        xlabel = "",
-        ylabel = "Percentage of mitochondrial\ngene counts",
+    sns.displot(data=adata.obs["total_counts"], bins=100, kde=False).set(
+        xlabel="Total counts per cell", ylabel="Number of cells"
+    ).savefig(f"{output_folder_path}/histo_raw_total_counts.svg")
+    relative_mt_counts_plot = sc.pl.violin(
+        adata=adata,
+        xlabel="",
+        ylabel="Percentage of mitochondrial\ngene counts",
         keys="pct_counts_mt",
-        show = False,
-        save="_raw_mitochondiral_counts.svg"
+        show=False,
+    )
+    relative_mt_counts_plot.figure.savefig(
+        f"{output_folder_path}/violin_raw_mitochondiral_counts.svg"
     )
     n_genes_plot_raw = sc.pl.scatter(
-        adata = adata,
-        x = "total_counts",
-        y = "n_genes_by_counts",
-        color = "pct_counts_mt",
-        title = "Percentage of mitochondrial gene counts",
-        show = False
+        adata=adata,
+        x="total_counts",
+        y="n_genes_by_counts",
+        color="pct_counts_mt",
+        title="Percentage of mitochondrial gene counts",
+        show=False,
     )
     n_genes_plot_raw.set(
-        xlabel ="Number of genes per cell",
-        ylabel = "Total counts per cell"
+        xlabel="Number of genes per cell", ylabel="Total counts per cell"
     )
-    n_genes_plot_raw.figure.savefig(f"{MOUNT_PATHS['output']}/scatter_raw_number_of_genes.svg")
+    n_genes_plot_raw.figure.savefig(
+        f"{output_folder_path}/scatter_raw_number_of_genes.svg"
+    )
 
     print("\tCalculating outliers...")
     adata.obs["outlier"] = (
@@ -106,24 +103,54 @@ def process_data(file_path):
     )
     n_outliers_mt = adata.obs.mt_outlier[adata.obs.mt_outlier].count()
 
-    print(f"\tNumber of cells before filtering: {adata.n_obs}")
+    n_cells_raw = adata.n_obs
+    print(f"\tNumber of cells before filtering: {n_cells_raw}")
     adata = adata[(~adata.obs.outlier) & (~adata.obs.mt_outlier)].copy()
+    n_cells_filtered = adata.n_obs
+    print(f"\tNumber of cells after filtering: {n_cells_filtered}")
 
-    print(f"\tNumber of cells after filtering: {adata.n_obs}")
+    print("\tWriting metrics to file...")
+    metrics_writer.writerow(
+        [file_path, n_cells_raw, n_cells_filtered, n_outliers, n_outliers_mt]
+    )
 
     print("\tPlotting filtered data...")
     n_genes_plot_filtered = sc.pl.scatter(
-        adata = adata,
-        x = "total_counts",
-        y = "n_genes_by_counts",
-        color = "pct_counts_mt",
-        title = "Percentage of mitochondrial gene counts",
-        show = False
+        adata=adata,
+        x="total_counts",
+        y="n_genes_by_counts",
+        color="pct_counts_mt",
+        title="Percentage of mitochondrial gene counts",
+        show=False,
     )
     n_genes_plot_filtered.set(
-        xlabel ="Number of genes per cell",
-        ylabel = "Total counts per cell"
+        xlabel="Number of genes per cell", ylabel="Total counts per cell"
     )
-    n_genes_plot_filtered.figure.savefig(f"{MOUNT_PATHS['output']}/scatter_filtered_number_of_genes.svg")
+    n_genes_plot_filtered.figure.savefig(
+        f"{output_folder_path}/scatter_filtered_number_of_genes.svg"
+    )
 
-process_data(f"{INPUT_FOLDER}/sample_filtered_feature_bc_matrix.h5")
+
+with open(f"{MOUNT_PATHS['output']}/metrics.csv", mode="w", newline="") as csvfile:
+    metrics_writer = csv.writer(
+        csvfile, dialect="unix", delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
+    )
+    metrics_writer.writerow(
+        [
+            "Sample",
+            "Number of cells before filtering",
+            "Number of cells after filtering",
+            "Number of QC outlier cells",
+            "Number of mitochondiral count outlier cells",
+        ]
+    )
+    # Iterates over all sample directories and processes them conserving the directory structure.
+    for root, dirs, files in os.walk(INPUT_FOLDER):
+        for file in files:
+            if file.casefold().endswith("filtered_feature_bc_matrix.h5"):
+                input_file_path = os.path.join(root, file)
+                output_folder_path = os.path.join(
+                    MOUNT_PATHS["output"], root.removeprefix(INPUT_FOLDER)
+                )
+                os.makedirs(output_folder_path, exist_ok=True)
+                process_data(input_file_path, output_folder_path, metrics_writer)
