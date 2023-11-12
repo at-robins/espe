6366eca21e47fa28cb8b9e0c0fa3ee191b4f5c1b
at-robins <30956028+at-robins@users.noreply.github.com>
2023-11-12 13:42:31 +0100

    [22] Removed separate installs
    Separate R package installs were removed.

--- pipelines/wiedemann_scrna_pipeline/container/ambient_rna/install.r
+++ pipelines/wiedemann_scrna_pipeline/container/ambient_rna/install.r
@@ -1,11 +1,4 @@
+# The library calls verify that the installation worked.
 repo <- "https://cloud.r-project.org/"
-
-if (!require("BiocManager", quietly = TRUE)) {
-    install.packages("BiocManager")
-}
-
-BiocManager::install("multtest")
-
-install.packages("jsonlite", repos = repo)
-install.packages("Seurat", repos = repo)
 install.packages("SoupX", repos = repo)
+library(SoupX)
