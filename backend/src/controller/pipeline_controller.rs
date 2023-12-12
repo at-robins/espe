e08793dd54c80a232d0ce61cb8fa930e5109951c
Robin <robin.schenk@tum.de>
2023-12-12 13:33:30 +0100

    [22] Removed old testing endpoint
    An old testing endpoint has been removed.

--- backend/src/controller/pipeline_controller.rs
+++ backend/src/controller/pipeline_controller.rs
@@ -1,103 +1,13 @@
-use actix_web::{web, HttpResponse, Responder};
-use chrono::Utc;
-use rand::{thread_rng, Rng};
+use actix_web::{web, HttpResponse};
 
 use crate::{
     application::{
         config::Configuration,
         error::{SeqError, SeqErrorType},
     },
-    model::{
-        exchange::pipeline_step_details::PipelineStepDetails,
-        internal::{pipeline_blueprint::PipelineBlueprint, step::PipelineStepStatus},
-    },
+    model::internal::pipeline_blueprint::PipelineBlueprint,
     service::pipeline_service::LoadedPipelines,
 };
-
-pub async fn get_pipeline_instance(wrapped_id: web::Path<u64>) -> Result<impl Responder, SeqError> {
-    let _id = wrapped_id.into_inner();
-    let dummy_response: Vec<PipelineStepDetails> = vec![
-        PipelineStepDetails {
-            id: 0,
-            name: "FASTQ-QC".to_string(),
-            status: random_status().to_string(),
-            creation_time: Utc::now(),
-        },
-        PipelineStepDetails {
-            id: 1,
-            name: "Trimming".to_string(),
-            status: random_status().to_string(),
-            creation_time: Utc::now(),
-        },
-        PipelineStepDetails {
-            id: 2,
-            name: "FASTQ-QC".to_string(),
-            status: random_status().to_string(),
-            creation_time: Utc::now(),
-        },
-        PipelineStepDetails {
-            id: 3,
-            name: "Alignment".to_string(),
-            status: random_status().to_string(),
-            creation_time: Utc::now(),
-        },
-        PipelineStepDetails {
-            id: 4,
-            name: "SAM-BAM-conversion".to_string(),
-            status: random_status().to_string(),
-            creation_time: Utc::now(),
-        },
-        PipelineStepDetails {
-            id: 5,
-            name: "BAM indexing".to_string(),
-            status: random_status().to_string(),
-            creation_time: Utc::now(),
-        },
-        PipelineStepDetails {
-            id: 6,
-            name: "BAM sorting".to_string(),
-            status: random_status().to_string(),
-            creation_time: Utc::now(),
-        },
-        PipelineStepDetails {
-            id: 7,
-            name: "Remove duplicates".to_string(),
-            status: random_status().to_string(),
-            creation_time: Utc::now(),
-        },
-        PipelineStepDetails {
-            id: 8,
-            name: "Remove mitochondrial reads".to_string(),
-            status: random_status().to_string(),
-            creation_time: Utc::now(),
-        },
-        PipelineStepDetails {
-            id: 9,
-            name: "Remove blacklisted regions".to_string(),
-            status: random_status().to_string(),
-            creation_time: Utc::now(),
-        },
-        PipelineStepDetails {
-            id: 10,
-            name: "Shift reads".to_string(),
-            status: random_status().to_string(),
-            creation_time: Utc::now(),
-        },
-        PipelineStepDetails {
-            id: 11,
-            name: "Post-alignment QC".to_string(),
-            status: random_status().to_string(),
-            creation_time: Utc::now(),
-        },
-        PipelineStepDetails {
-            id: 12,
-            name: "Peak calling".to_string(),
-            status: random_status().to_string(),
-            creation_time: Utc::now(),
-        },
-    ];
-    Ok(web::Json(dummy_response))
-}
 
 /// Return all pipeline blueprints that are currently loaded.
 pub async fn get_pipeline_blueprints(
@@ -137,14 +47,4 @@ pub async fn patch_pipeline_blueprints(
 ) -> Result<HttpResponse, SeqError> {
     pipelines.update_loaded_pipelines(app_config)?;
     Ok(HttpResponse::Ok().finish())
-}
-
-fn random_status() -> PipelineStepStatus {
-    match thread_rng().gen_range(0..4) {
-        0 => PipelineStepStatus::Failed,
-        1 => PipelineStepStatus::Pending,
-        2 => PipelineStepStatus::Running,
-        3 => PipelineStepStatus::Success,
-        _ => PipelineStepStatus::Failed,
-    }
 }
