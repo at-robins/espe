37c82ab49c723186f9e54b0695e3bd253b7cf051
Robin <robin.schenk@tum.de>
2024-03-05 15:43:02 +0100

    [26] Added build registry model
    A model for the build registry database interaction has been added.

--- backend/migrations/2024-03-05-124412_container_build_versioning/up.sql
+++ backend/migrations/2024-03-05-124412_container_build_versioning/up.sql
@@ -3,5 +3,6 @@ CREATE TABLE pipeline_build_register (
     pipeline_id TEXT NOT NULL,
     pipeline_step_id TEXT NOT NULL,
     pipeline_version TEXT NOT NULL,
-    creation_time DATETIME NOT NULL
+    creation_time DATETIME NOT NULL,
+    UNIQUE(pipeline_id, pipeline_step_id)
 );
