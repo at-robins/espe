583968e55a742cad22f336f255b90d880dddce0c
at-robins <30956028+at-robins@users.noreply.github.com>
2024-03-12 20:24:52 +0100

    [21] Added ATAC specific shift
    The ATAC specific read shift has been added to seqoutbias.

--- pipelines/wiedemann_atac_pipeline/container/seqoutbias/run_seqout.py
+++ pipelines/wiedemann_atac_pipeline/container/seqoutbias/run_seqout.py
@@ -54,13 +54,13 @@ for root, dirs, files in os.walk(INPUT_FOLDER):
                 f"samtools view -@ {threads} -bh -F 16 {file_base_path}{BAM_SUFFIX} -o {bam_path_plus}",
                 f"samtools view -@ {threads} -bh -f 16 {file_base_path}{BAM_SUFFIX} -o {bam_path_minus}",
                 (
-                    f"seqOutBias {GENOME_PATH} {bam_path_plus} "
+                    f"seqOutBias {GENOME_PATH} {bam_path_plus} --custom-shift=4,-5 "
                     f"--bw={bw_name_plus} --out={SEQTABLE_PATH_PLUS} "
                     f"--skip-bed --shift-counts --kmer-mask {STRAND_MASK_PLUS} "
                     f"--read-size={READ_SIZE} --tallymer={TALLYMER_PATH}"
                 ),
                 (
-                    f"seqOutBias {GENOME_PATH} {bam_path_minus} "
+                    f"seqOutBias {GENOME_PATH} {bam_path_minus} --custom-shift=4,-5 "
                     f"--bw={bw_name_minus} --out={SEQTABLE_PATH_MINUS} "
                     f"--skip-bed --shift-counts --kmer-mask {STRAND_MASK_MINUS} "
                     f"--read-size={READ_SIZE} --tallymer={TALLYMER_PATH}"
