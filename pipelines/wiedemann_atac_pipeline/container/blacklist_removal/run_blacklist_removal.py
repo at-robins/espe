c74bc434d26c5d272e0110e7edc3b2c64a6c1bee
Robin <robin.schenk@tum.de>
2024-03-25 18:04:05 +0100

    [21] WIP update
    The seqOutBias step has benn removed.

--- pipelines/wiedemann_atac_pipeline/container/blacklist_removal/run_blacklist_removal.py
+++ pipelines/wiedemann_atac_pipeline/container/blacklist_removal/run_blacklist_removal.py
@@ -2,22 +2,26 @@
 """This module removes blacklisted regions."""
 
 import json
+import math
+import multiprocessing
 import os
 import subprocess
 
 MOUNT_PATHS = json.loads(os.environ.get("MOUNT_PATHS"))
-INPUT_FOLDER = MOUNT_PATHS["dependencies"]["seqoutbias"]
+INPUT_FOLDER = MOUNT_PATHS["dependencies"]["splitting"]
 BLACKLIST_PATH = os.path.join(MOUNT_PATHS["globals"]["GENOME"], "blacklist.bed")
-CHROM_SIZES_PATH = os.path.join(MOUNT_PATHS["globals"]["GENOME"], "genome.chrom.sizes")
 BAM_SUFFIX = ".bam"
-BED_SUFFIX = ".bed"
+
+threads = math.floor(multiprocessing.cpu_count() * 0.8)
+if threads < 1:
+    threads = 1
 
 # Iterates over all sample directories and processes them conserving the directory structure.
 for root, dirs, files in os.walk(INPUT_FOLDER):
     for file in files:
-        if file.endswith(BED_SUFFIX):
-            print(f"Processing file {os.path.join(root, file)}...")
-            file_base_name = file.removesuffix(BED_SUFFIX)
+        if file.endswith(BAM_SUFFIX):
+            print(f"Processing file {os.path.join(root, file)}...", flush=True)
+            file_base_name = file.removesuffix(BAM_SUFFIX)
             file_base_path = os.path.join(root, file_base_name)
             file_base_output_path = os.path.join(
                 MOUNT_PATHS["output"],
@@ -25,21 +29,24 @@ for root, dirs, files in os.walk(INPUT_FOLDER):
             )
             os.makedirs(os.path.dirname(file_base_output_path), exist_ok=True)
 
-            print("Removing blacklisted regions...")
+            print("Removing blacklisted regions...", flush=True)
             subprocess.run(
                 (
-                    f"bedtools intersect -a {file_base_path}{BED_SUFFIX} -b {BLACKLIST_PATH} -wa -v "
-                    f"> {file_base_output_path}{BED_SUFFIX}"
+                    f"bedtools intersect -abam {file_base_path}{BAM_SUFFIX} -b {BLACKLIST_PATH} -wa -v "
+                    f"| samtools sort -@ {threads} -O bam -o {file_base_output_path}{BAM_SUFFIX} -"
                 ),
                 shell=True,
                 check=True,
             )
-
-            print("Converting to bam files...")
+            subprocess.run(
+                f"samtools index -@ {threads} {file_base_output_path}{BAM_SUFFIX}",
+                shell=True,
+                check=True,
+            )
             subprocess.run(
                 (
-                    f"bedtools bedtobam -i {file_base_output_path}{BED_SUFFIX} -g {CHROM_SIZES_PATH} "
-                    f"> {file_base_output_path}{BAM_SUFFIX}"
+                    f"samtools flagstat -@ {threads} -O json {file_base_output_path}{BAM_SUFFIX} > "
+                    f"{file_base_output_path}.flagstat"
                 ),
                 shell=True,
                 check=True,
--- pipelines/wiedemann_genome_preprocess/container/unpack/run.py
+++ pipelines/wiedemann_genome_preprocess/container/unpack/run.py
@@ -10,6 +10,7 @@ import sys
 MOUNT_PATHS = json.loads(os.environ.get("MOUNT_PATHS"))
 TEMPORARY_OUTPUT_PATH = os.path.join(MOUNT_PATHS["output"], "tmp")
 DOWNLOAD_PATH_GENOME = os.path.join(TEMPORARY_OUTPUT_PATH, "genome.zip")
+GENOME_INFO_DIRECTORY = "/genome_info"
 
 # Setup.
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s: %(message)s")
@@ -35,27 +36,44 @@ exit_code_unzip_genome = os.waitstatus_to_exitcode(os.system(command_unzip_genom
 if exit_code_unzip_genome != 0:
     sys.exit(exit_code_unzip_genome)
 
-logging.info("Renaming files...")
-os.rename(
-    os.path.join(
-        TEMPORARY_OUTPUT_PATH,
-        "ncbi_dataset",
-        "data",
-        "GCF_000001635.26",
-        "GCF_000001635.26_GRCm38.p6_genomic.fna",
-    ),
-    os.path.join(MOUNT_PATHS["output"], "genome.fa"),
+logging.info("Renaming files and chromosomes...")
+genome_fasta_path_in = os.path.join(
+    TEMPORARY_OUTPUT_PATH,
+    "ncbi_dataset",
+    "data",
+    "GCF_000001635.26",
+    "GCF_000001635.26_GRCm38.p6_genomic.fna",
 )
-os.rename(
-    os.path.join(
-        TEMPORARY_OUTPUT_PATH,
-        "ncbi_dataset",
-        "data",
-        "GCF_000001635.26",
-        "genomic.gff",
-    ),
-    os.path.join(MOUNT_PATHS["output"], "annotations.gff"),
+genome_annotations_path_oin = os.path.join(
+    TEMPORARY_OUTPUT_PATH,
+    "ncbi_dataset",
+    "data",
+    "GCF_000001635.26",
+    "genomic.gff",
 )
+genome_fasta_path_out = os.path.join(MOUNT_PATHS["output"], "genome.fa")
+genome_annotations_path_out = os.path.join(MOUNT_PATHS["output"], "annotations.gff")
+
+with open("log.txt") as infile:
+    for line in infile:
+        print(line)
+
+logging.info("Renaming chromosomes...")
+genome_info_path = os - path.join(GENOME_INFO_DIRECTORY, "chromosome_names_mouse.tsv")
+with open(genome_info_path, newline="", encoding="utf-8") as tsvfile:
+    info_reader = csv.DictReader(tsvfile, dialect="unix", delimiter="\t", quotechar='"')
+    for row in info_reader:
+        old_chrom_name = row["RefSeq seq accession"]
+        new_chrom_name = row["UCSC style name"]
+        if old_chrom_name and new_chrom_name:
+            logging.info("\tRenaming {old_chrom_name} to {new_chrom_name}...")
+
+        sample_type = sample_info_map.get(row["sample type"])
+        sample_directory = row["sample directory"]
+        if sample_type is None:
+            sample_info_map[row["sample type"]] = [sample_directory]
+        else:
+            sample_type.append(sample_directory)
 
 logging.info("Compressing files...")
 command_zip = f"gzip -k {os.path.join(MOUNT_PATHS['output'], 'genome.fa')}"
