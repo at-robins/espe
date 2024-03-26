0edb253fe850d8ca27ae47bd0a1313fbfa95121f
at-robins <30956028+at-robins@users.noreply.github.com>
2024-03-26 15:39:03 +0100

    [21] Update peak calling metrics
    Cutoff metrics have been added to the peak calling step.

--- pipelines/wiedemann_atac_pipeline/container/peak_calling/run_peak_calling.py
+++ pipelines/wiedemann_atac_pipeline/container/peak_calling/run_peak_calling.py
@@ -15,7 +15,7 @@ with open(GENOME_SIZE_PATH, mode="rt", encoding="utf-8") as g_file:
     mappable_genome_size = g_file.read()
     print(f"Mappable genome size: {mappable_genome_size}")
 
-options = f"-f BAMPE -q 0.05 -B -g {mappable_genome_size}"
+options = f"-f BAMPE -q 0.05 --cutoff-analysis -B -g {mappable_genome_size}"
 
 # Iterates over all sample directories and processes them conserving the directory structure.
 FILE_EXTENSION = ".bam"
