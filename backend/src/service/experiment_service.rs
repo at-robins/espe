a68d0eafc828f5343dca2ed29b9b01082fbcba44
at-robins <30956028+at-robins@users.noreply.github.com>
2024-06-18 10:40:08 +0200

    [29] Added tests
    Test for the new functions have been added.

--- backend/src/service/experiment_service.rs
+++ backend/src/service/experiment_service.rs
@@ -203,8 +203,252 @@ pub fn open_step_log<P: AsRef<str>, S: AsRef<str>>(
 
 #[cfg(test)]
 mod tests {
+    use std::path::Path;
+
+    use crate::test_utility::TestContext;
+
     use super::*;
 
+    /// Creates a dummy file at the specified path with dummy content.
+    ///
+    /// # Parameters
+    ///
+    /// * `file_path` - the [`Path`] to the file
+    fn create_dummy_file<P: AsRef<Path>>(file_path: P) {
+        if let Some(parent_path) = file_path.as_ref().parent() {
+            std::fs::create_dir_all(parent_path).unwrap();
+        }
+        std::fs::write(file_path, "dummy content".as_bytes()).unwrap();
+    }
+
     #[test]
-    fn test_() {}
+    fn test_delete_step_output() {
+        let context = TestContext::new();
+        let app_config: Configuration = (&context).into();
+        let experiment_id = 42;
+        let step_id = "123456789";
+        let output_path = app_config.experiment_step_path(experiment_id.to_string(), step_id);
+        let test_output_path = output_path.join("test_dir/test.txt");
+        create_dummy_file(&test_output_path);
+        assert!(output_path.exists());
+        assert!(test_output_path.exists());
+        delete_step_output(step_id, experiment_id, web::Data::new(app_config)).unwrap();
+        assert!(!output_path.exists());
+        assert!(!test_output_path.exists());
+    }
+
+    #[test]
+    fn test_delete_step_logs() {
+        let context = TestContext::new();
+        let app_config: Configuration = (&context).into();
+        let experiment_id = 42;
+        let step_id = "123456789";
+        let pipeline_id = "test_pipeline";
+        let build_log_paths = app_config.experiment_log_paths(
+            experiment_id.to_string(),
+            &pipeline_id,
+            &step_id,
+            &[LogProcessType::Build],
+        );
+        let run_log_paths = app_config.experiment_log_paths(
+            experiment_id.to_string(),
+            &pipeline_id,
+            &step_id,
+            &[LogProcessType::Run],
+        );
+        // Creates run and build log files.
+        for log_path in build_log_paths.iter().chain(run_log_paths.iter()) {
+            create_dummy_file(log_path);
+        }
+        // Deletes the run log files.
+        delete_step_logs(
+            pipeline_id,
+            step_id,
+            experiment_id,
+            &[LogProcessType::Run],
+            web::Data::new(app_config),
+        )
+        .unwrap();
+        // Run logs should be deleted, while build logs should still exist.
+        for build_log_path in build_log_paths {
+            assert!(build_log_path.exists());
+        }
+        for run_log_path in run_log_paths {
+            assert!(!run_log_path.exists());
+        }
+    }
+
+    #[test]
+    fn test_create_run_context() {
+        let context = TestContext::new();
+        let app_config: Configuration = (&context).into();
+        let experiment_id = 42;
+        let step_id = "123456789";
+
+        let input_path = app_config.experiment_input_path(experiment_id.to_string());
+        let output_path = app_config.experiment_step_path(experiment_id.to_string(), step_id);
+        let logs_path = app_config.experiment_logs_path(experiment_id.to_string());
+
+        assert!(!input_path.exists());
+        assert!(!output_path.exists());
+        assert!(!logs_path.exists());
+        create_run_context(step_id, experiment_id, web::Data::new(app_config)).unwrap();
+        assert!(input_path.exists());
+        assert!(output_path.exists());
+        assert!(logs_path.exists());
+    }
+
+    #[test]
+    fn test_create_build_context() {
+        let context = TestContext::new();
+        let app_config: Configuration = (&context).into();
+        let experiment_id = 42;
+        let logs_path = app_config.experiment_logs_path(experiment_id.to_string());
+
+        assert!(!logs_path.exists());
+        create_build_context(experiment_id, web::Data::new(app_config)).unwrap();
+        assert!(logs_path.exists());
+    }
+
+    #[test]
+    fn test_prepare_context_for_run_empty() {
+        let context = TestContext::new();
+        let app_config: Configuration = (&context).into();
+        let experiment_id = 42;
+        let step_id = "123456789";
+        let pipeline_id = "test_pipeline";
+
+        let input_path = app_config.experiment_input_path(experiment_id.to_string());
+        let output_path = app_config.experiment_step_path(experiment_id.to_string(), step_id);
+        let logs_path = app_config.experiment_logs_path(experiment_id.to_string());
+
+        assert!(!input_path.exists());
+        assert!(!output_path.exists());
+        assert!(!logs_path.exists());
+
+        prepare_context_for_run(pipeline_id, step_id, experiment_id, web::Data::new(app_config))
+            .unwrap();
+
+        assert!(input_path.exists());
+        assert!(output_path.exists());
+        assert!(logs_path.exists());
+    }
+
+    #[test]
+    fn test_prepare_context_for_run_filled() {
+        let context = TestContext::new();
+        let app_config: Configuration = (&context).into();
+        let experiment_id = 42;
+        let step_id = "123456789";
+        let pipeline_id = "test_pipeline";
+
+        let input_path = app_config.experiment_input_path(experiment_id.to_string());
+        let output_path = app_config.experiment_step_path(experiment_id.to_string(), step_id);
+        let logs_path = app_config.experiment_logs_path(experiment_id.to_string());
+        let build_log_paths = app_config.experiment_log_paths(
+            experiment_id.to_string(),
+            &pipeline_id,
+            &step_id,
+            &[LogProcessType::Build],
+        );
+        let run_log_paths = app_config.experiment_log_paths(
+            experiment_id.to_string(),
+            &pipeline_id,
+            &step_id,
+            &[LogProcessType::Run],
+        );
+
+        // Dummy files.
+        let step_file_path = output_path.join("dummy.txt");
+        create_dummy_file(&step_file_path);
+        // Creates run and build log files.
+        for log_path in build_log_paths.iter().chain(run_log_paths.iter()) {
+            create_dummy_file(log_path);
+        }
+
+        prepare_context_for_run(pipeline_id, step_id, experiment_id, web::Data::new(app_config))
+            .unwrap();
+
+        // All required directories should exist...
+        assert!(input_path.exists());
+        assert!(output_path.exists());
+        assert!(logs_path.exists());
+        // ... but old files should be deleted.
+        assert!(!step_file_path.exists());
+        // Run logs should be deleted, while build logs should still exist.
+        for build_log_path in build_log_paths {
+            assert!(build_log_path.exists());
+        }
+        for run_log_path in run_log_paths {
+            assert!(!run_log_path.exists());
+        }
+    }
+
+    #[test]
+    fn test_prepare_context_for_build_empty() {
+        let context = TestContext::new();
+        let app_config: Configuration = (&context).into();
+        let experiment_id = 42;
+        let step_id = "123456789";
+        let pipeline_id = "test_pipeline";
+
+        let logs_path = app_config.experiment_logs_path(experiment_id.to_string());
+
+        assert!(!logs_path.exists());
+
+        prepare_context_for_build(pipeline_id, step_id, experiment_id, web::Data::new(app_config))
+            .unwrap();
+
+        assert!(logs_path.exists());
+    }
+
+    #[test]
+    fn test_prepare_context_for_build_filled() {
+        let context = TestContext::new();
+        let app_config: Configuration = (&context).into();
+        let experiment_id = 42;
+        let step_id = "123456789";
+        let pipeline_id = "test_pipeline";
+
+        let input_path = app_config.experiment_input_path(experiment_id.to_string());
+        let output_path = app_config.experiment_step_path(experiment_id.to_string(), step_id);
+        let logs_path = app_config.experiment_logs_path(experiment_id.to_string());
+        let build_log_paths = app_config.experiment_log_paths(
+            experiment_id.to_string(),
+            &pipeline_id,
+            &step_id,
+            &[LogProcessType::Build],
+        );
+        let run_log_paths = app_config.experiment_log_paths(
+            experiment_id.to_string(),
+            &pipeline_id,
+            &step_id,
+            &[LogProcessType::Run],
+        );
+
+        // Dummy files.
+        let step_file_path = output_path.join("dummy.txt");
+        create_dummy_file(&step_file_path);
+        // Creates run and build log files.
+        for log_path in build_log_paths.iter().chain(run_log_paths.iter()) {
+            create_dummy_file(log_path);
+        }
+
+        prepare_context_for_build(pipeline_id, step_id, experiment_id, web::Data::new(app_config))
+            .unwrap();
+
+        // Only the log path should be created...
+        assert!(!input_path.exists());
+        assert!(logs_path.exists());
+        // ... and old output files should still exist.
+        assert!(output_path.exists());
+        assert!(step_file_path.exists());
+        // Build logs should be deleted, while run logs should still exist.
+        for build_log_path in build_log_paths {
+            assert!(!build_log_path.exists());
+        }
+        for run_log_path in run_log_paths {
+            assert!(run_log_path.exists());
+        }
+    }
 }
