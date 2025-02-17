b77d2d9b923724f19a6d742cf768d1cf62153ee0
at-robins <30956028+at-robins@users.noreply.github.com>
2025-02-17 09:36:52 +0100

    [21] Added clustermap plotting
    Added clustermaps to plot the correlation matrix.

--- pipelines/wiedemann_atac_pipeline/container/motif_association/run_association.py
+++ pipelines/wiedemann_atac_pipeline/container/motif_association/run_association.py
@@ -6,6 +6,7 @@ import json
 import numpy as np
 import os
 import pandas as pd
+import seaborn as sns
 
 from pathlib import Path
 
@@ -21,7 +22,39 @@ KEY_ANNOTATION_GENE_NAME = "Gene Name"
 
 
 def filter_genes_on_motif(group, filter_motif):
+    """
+    Returns true if the group contains the specified motif.
+    """
     return filter_motif in group[KEY_MOTIF_NAME].values
+
+
+def plot_correlation_clustermap(cm_data, cm_output_path, cluster_rows, cluster_columns):
+    """
+    Plots the correlation clustermaps.
+    """
+    # The diagonal might have extreme values, so median is better than mean.
+    matrix_median = np.median(cm_data.values)
+    # We want the actual standard deveation as we are looking at the whole population.
+    matrix_sd = np.std(cm_data.values, ddof=0)
+    upper_colour_cutoff = matrix_median + 3 * matrix_sd
+    plot = sns.clustermap(
+        cm_data,
+        cbar_kws={"label": "motif correlation"},
+        cmap="vlag",
+        vmin=0,
+        vmax=upper_colour_cutoff,
+        center=1,
+        row_cluster=cluster_rows,
+        col_cluster=cluster_columns,
+        figsize=(
+            6.0 + 0.6 * len(cm_data.columns),
+            4.0 + 0.3 * len(cm_data.index),
+        ),
+    )
+    plot.ax_row_dendrogram.set_visible(False)
+    plot.ax_col_dendrogram.set_visible(False)
+    plot.ax_heatmap.grid(False)
+    plot.savefig(cm_output_path)
 
 
 print("Loading annotations...", flush=True)
@@ -39,19 +72,41 @@ for root, dirs, files in os.walk(INPUT_FOLDER_MOTIF_DISCOVERY):
             )
             os.makedirs(output_folder_path, exist_ok=True)
             annotated_path = Path(
-                os.path.join(output_folder_path, f"annotated_motif_association.csv")
+                os.path.join(output_folder_path, "annotated_motif_association.csv")
             )
             correlation_path_abs = Path(
                 os.path.join(
-                    output_folder_path, f"motif_correlation_matrix_absolute.csv"
+                    output_folder_path, "motif_correlation_matrix_absolute.csv"
                 )
             )
             correlation_path_rel = Path(
                 os.path.join(
-                    output_folder_path, f"motif_correlation_matrix_relative.csv"
+                    output_folder_path, "motif_correlation_matrix_relative.csv"
                 )
             )
-            print(f"Annotating {raw_path}...", flush=True)
+            clustermap_path_none = Path(
+                os.path.join(
+                    output_folder_path, "motif_correlation_relative_clustermap_none.svg"
+                )
+            )
+            clustermap_path_both = Path(
+                os.path.join(
+                    output_folder_path, "motif_correlation_relative_clustermap_both.svg"
+                )
+            )
+            clustermap_path_row = Path(
+                os.path.join(
+                    output_folder_path, "motif_correlation_relative_clustermap_row.svg"
+                )
+            )
+            clustermap_path_col = Path(
+                os.path.join(
+                    output_folder_path,
+                    "motif_correlation_relative_clustermap_column.svg",
+                )
+            )
+            print(f"Processing {raw_path}...")
+            print("\tAnnotating motifs...", flush=True)
             motif_table = pd.read_csv(
                 raw_path, sep="\t", header=0, index_col=False, encoding="utf-8"
             )
@@ -62,13 +117,14 @@ for root, dirs, files in os.walk(INPUT_FOLDER_MOTIF_DISCOVERY):
                 right_index=True,
                 how="left",
             ).sort_values(KEY_ANNOTATION_GENE_NAME, ascending=True)
-            # Writes annotated motifs to CSV.
+            print("\tSaving annotated motifs...", flush=True)
             merged_motifs.to_csv(
                 annotated_path,
                 sep=",",
                 encoding="utf-8",
                 index=False,
             )
+            print("\tCreating motif correlation matrices...", flush=True)
             merged_motifs.dropna(
                 axis=0, inplace=True, subset=[KEY_ANNOTATION_GENE_NAME, KEY_MOTIF_NAME]
             )
@@ -97,6 +153,12 @@ for root, dirs, files in os.walk(INPUT_FOLDER_MOTIF_DISCOVERY):
                     motif_correlation_df_abs[motif_a] / motif_background_frequency
                 )
 
+            # Empty values are the expected results of motifs not being present
+            # in a motif subset, so these values should be filled with zeros.
+            motif_correlation_df_abs.fillna(0, inplace=True)
+            motif_correlation_df_rel.fillna(0, inplace=True)
+
+            print("\tSaving motif correlation matrices...", flush=True)
             # Saves absolute correlation values.
             motif_correlation_df_abs.to_csv(
                 correlation_path_abs,
@@ -110,4 +172,30 @@ for root, dirs, files in os.walk(INPUT_FOLDER_MOTIF_DISCOVERY):
                 sep=",",
                 encoding="utf-8",
                 index=True,
+            )
+
+            print("\tPlotting motif correlation matrices...", flush=True)
+            plot_correlation_clustermap(
+                cm_data=motif_correlation_df_rel,
+                cm_output_path=clustermap_path_none,
+                cluster_rows=False,
+                cluster_columns=False,
+            )
+            plot_correlation_clustermap(
+                cm_data=motif_correlation_df_rel,
+                cm_output_path=clustermap_path_row,
+                cluster_rows=True,
+                cluster_columns=False,
+            )
+            plot_correlation_clustermap(
+                cm_data=motif_correlation_df_rel,
+                cm_output_path=clustermap_path_col,
+                cluster_rows=False,
+                cluster_columns=True,
+            )
+            plot_correlation_clustermap(
+                cm_data=motif_correlation_df_rel,
+                cm_output_path=clustermap_path_both,
+                cluster_rows=True,
+                cluster_columns=True,
             )
