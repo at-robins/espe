41b3bc3abe09bd5ca44aa4927aa0154b76d989d0
Robin <robin.schenk@tum.de>
2025-04-22 16:23:55 +0200

    [21] Split motif discovery
    Motif discovery was split into more an less accessible regions.

--- pipelines/wiedemann_atac_pipeline/container/motif_discovery/run_motif_discovery.py
+++ pipelines/wiedemann_atac_pipeline/container/motif_discovery/run_motif_discovery.py
@@ -12,6 +12,7 @@ MOUNT_PATHS = json.loads(os.environ.get("MOUNT_PATHS"))
 INPUT_FOLDER = next(iter(MOUNT_PATHS["dependencies"].values()))
 MERGED_MOTIF_FILE_NAME = "merged.motif"
 MOTIF_ANNOTATED_PEAK_FILE_NAME = "motif_annotated_peaks.tsv"
+PEAK_REGION_FILE_NAME = "differentially_accessible_peaks.tsv"
 
 threads = math.floor(multiprocessing.cpu_count() * 0.8)
 if threads < 1:
@@ -37,6 +38,83 @@ def merge_motifs(motif_input_folder):
                 )
 
 
+def filter_p(pvalue):
+    """
+    Applies the p-value filter.
+    """
+    return pvalue != "" and float(pvalue) <= 0.05
+
+
+def fitler_lfc(lfc, filter):
+    """
+    Applies the log-2-fold-change filter if set.
+    """
+    return (
+        filter is None
+        or (lfc != "" and float(lfc) >= 0.0 and filter == "+")
+        or (lfc != "" and float(lfc) <= 0.0 and filter == "-")
+    )
+
+
+def create_region_file(input_file_path, output_directory_path, filter=None):
+    if filter == "+":
+        output_directory_path_final = os.path.join(
+            output_directory_path, "more accessible regions"
+        )
+    elif filter == "-":
+        output_directory_path_final = os.path.join(
+            output_directory_path, "less accessible regions"
+        )
+    else:
+        output_directory_path_final = os.path.join(output_directory_path, "all regions")
+
+    os.makedirs(output_directory_path_final, exist_ok=True)
+    file_path_peaks = os.path.join(output_directory_path_final, PEAK_REGION_FILE_NAME)
+
+    # Creates the input file with all differentially accessible peaks.
+    with open(
+        input_file_path, newline="", mode="rt", encoding="utf-8"
+    ) as annotated_file:
+        with open(
+            file_path_peaks, newline="", mode="wt", encoding="utf-8"
+        ) as peak_file:
+            annotated_reader = csv.DictReader(
+                annotated_file, dialect="unix", delimiter=",", quotechar='"'
+            )
+            peak_writer = csv.writer(
+                peak_file,
+                dialect="unix",
+                delimiter="\t",
+                quotechar='"',
+                quoting=csv.QUOTE_MINIMAL,
+            )
+            # Writes the header.
+            peak_writer.writerow(
+                [
+                    "PeakID",
+                    "Chr",
+                    "Start",
+                    "End",
+                    "Strand",
+                ]
+            )
+
+            for row in annotated_reader:
+                # Copies relevant values of each row.
+                if filter_p(pvalue=row["padj"]) and fitler_lfc(
+                    lfc=row["log2FoldChange"], filter=filter
+                ):
+                    peak_writer.writerow(
+                        [
+                            row["PeakID"],
+                            row["Chr"],
+                            row["Start"],
+                            row["End"],
+                            row["Strand"],
+                        ]
+                    )
+
+
 print("Loading genome database...")
 organism_env = os.environ.get("GLOBAL_ORGANISM")
 if organism_env is not None and organism_env == "human":
@@ -59,51 +137,27 @@ for root, dirs, files in os.walk(INPUT_FOLDER):
             directory_path_output = os.path.join(
                 MOUNT_PATHS["output"], output_directory_name
             )
-            os.makedirs(directory_path_output, exist_ok=True)
-            file_path_peaks = os.path.join(
-                directory_path_output, "differential_accessible_peaks.tsv"
+            create_region_file(
+                input_file_path=file_path_annotated,
+                output_directory_path=directory_path_output,
+                filter=None,
             )
-            # Creates the input file with all differentially accessible peaks.
-            with open(
-                file_path_annotated, newline="", mode="rt", encoding="utf-8"
-            ) as annotated_file:
-                with open(
-                    file_path_peaks, newline="", mode="wt", encoding="utf-8"
-                ) as peak_file:
-                    annotated_reader = csv.DictReader(
-                        annotated_file, dialect="unix", delimiter=",", quotechar='"'
-                    )
-                    peak_writer = csv.writer(
-                        peak_file,
-                        dialect="unix",
-                        delimiter="\t",
-                        quotechar='"',
-                        quoting=csv.QUOTE_MINIMAL,
-                    )
-                    # Writes the header.
-                    peak_writer.writerow(
-                        [
-                            "PeakID",
-                            "Chr",
-                            "Start",
-                            "End",
-                            "Strand",
-                        ]
-                    )
-                    for row in annotated_reader:
-                        # Copies relevant values of each row.
-                        if row["padj"] != "" and float(row["padj"]) <= 0.05:
-                            peak_writer.writerow(
-                                [
-                                    row["PeakID"],
-                                    row["Chr"],
-                                    row["Start"],
-                                    row["End"],
-                                    row["Strand"],
-                                ]
-                            )
+            create_region_file(
+                input_file_path=file_path_annotated,
+                output_directory_path=directory_path_output,
+                filter="+",
+            )
+            create_region_file(
+                input_file_path=file_path_annotated,
+                output_directory_path=directory_path_output,
+                filter="-",
+            )
 
-            print("\tRunning motif discovery...", flush=True)
+for directory_path_output, dirs, files in os.walk(MOUNT_PATHS["output"]):
+    for file in files:
+        if file == PEAK_REGION_FILE_NAME:
+            file_path_peaks = os.path.join(directory_path_output, file)
+            print(f"Running motif discovery for {file_path_peaks}...", flush=True)
             subprocess.run(
                 (
                     f'findMotifsGenome.pl "{file_path_peaks}" {genome_id} '
@@ -149,10 +203,15 @@ for root, dirs, files in os.walk(INPUT_FOLDER):
                     check=True,
                 )
             else:
-                print("\tNo known motifs discovered. Skipping peak annotation...", flush=True)
- 
+                print(
+                    "\tNo known motifs discovered. Skipping peak annotation...",
+                    flush=True,
+                )
+
             if os.path.isfile(file_denovo_motifs):
-                print("\tAnnotating peaks with discovered de novo motifs...", flush=True)
+                print(
+                    "\tAnnotating peaks with discovered de novo motifs...", flush=True
+                )
                 subprocess.run(
                     (
                         f'findMotifsGenome.pl "{file_path_peaks}" {genome_id} '
@@ -164,4 +223,7 @@ for root, dirs, files in os.walk(INPUT_FOLDER):
                     check=True,
                 )
             else:
-                print("\tNo de novo motifs discovered. Skipping peak annotation...", flush=True)
+                print(
+                    "\tNo de novo motifs discovered. Skipping peak annotation...",
+                    flush=True,
+                )
