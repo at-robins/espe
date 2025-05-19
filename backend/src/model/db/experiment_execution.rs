c95f1024fe8b3f2b36c33797bd3c11b93562e1be
at-robins <30956028+at-robins@users.noreply.github.com>
2025-05-19 08:57:44 +0200

    [19] Added function to check for executed experiments
    A function that checks if an experiment is currently sheduled for execution has been added.

--- backend/src/model/db/experiment_execution.rs
+++ backend/src/model/db/experiment_execution.rs
@@ -28,6 +28,10 @@ const EXECUTION_STATUS_FAILED: &str = "Failed";
 const EXECUTION_STATUS_FINISHED: &str = "Finished";
 const EXECUTION_STATUS_RUNNING: &str = "Running";
 const EXECUTION_STATUS_WAITING: &str = "Waiting";
+// const EXECUTION_STATI_NOT_FINISHED: [ExecutionStatus; 2] =
+//     [ExecutionStatus::Running, ExecutionStatus::Waiting];
+const EXECUTION_STATI_NOT_FINISHED: [&str; 2] =
+    [EXECUTION_STATUS_RUNNING, EXECUTION_STATUS_WAITING];
 
 #[derive(Debug, PartialEq)]
 /// The execution status of a pipeline step.
@@ -198,22 +202,41 @@ impl ExperimentExecution {
         connection: &mut SqliteConnection,
     ) -> Result<usize, diesel::result::Error> {
         let status: String = status.into();
-        let status_not_finished: Vec<String> = vec![
-            ExecutionStatus::Running.into(),
-            ExecutionStatus::Waiting.into(),
-        ];
         diesel::update(
             crate::schema::experiment_execution::table.filter(
                 crate::schema::experiment_execution::experiment_id
                     .eq(experiment_id)
                     .and(
                         crate::schema::experiment_execution::execution_status
-                            .eq_any(status_not_finished),
+                            .eq_any(EXECUTION_STATI_NOT_FINISHED),
                     ),
             ),
         )
         .set(crate::schema::experiment_execution::execution_status.eq(status))
         .execute(connection)
+    }
+
+    /// Returns `true` if the experiment is currently executed.
+    ///
+    /// # Parameters
+    ///
+    /// * `experiemnt_id` - the ID of the experiment
+    /// * `connection` - the database connection
+    pub fn is_executed(
+        experiment_id: i32,
+        connection: &mut SqliteConnection,
+    ) -> Result<bool, diesel::result::Error> {
+        diesel::select(diesel::dsl::exists(
+            crate::schema::experiment_execution::table.filter(
+                crate::schema::experiment_execution::experiment_id
+                    .eq(experiment_id)
+                    .and(
+                        crate::schema::experiment_execution::execution_status
+                            .eq_any(EXECUTION_STATI_NOT_FINISHED),
+                    ),
+            ),
+        ))
+        .get_result(connection)
     }
 }
 
@@ -660,5 +683,88 @@ mod tests {
             all_records_after_update[4].execution_status().unwrap(),
             ExecutionStatus::Failed
         );
+    }
+
+    #[test]
+    fn test_is_executed() {
+        // Use a reference to the context, so the context is not dropped early
+        // and messes up test context folder deletion.
+        let context = TestContext::new();
+        let mut connection = context.get_connection();
+        // Create an experiment containing all different stati.
+        let experiment_all = NewExperiment::new("all".to_string());
+        let experiment_waiting = NewExperiment::new("waiting".to_string());
+        let experiment_running = NewExperiment::new("running".to_string());
+        let experiment_not_executed = NewExperiment::new("not executed".to_string());
+        let experiment_empty = NewExperiment::new("empty".to_string());
+        let experiment_id_all: i32 = diesel::insert_into(crate::schema::experiment::table)
+            .values(&experiment_all)
+            .returning(crate::schema::experiment::id)
+            .get_result(&mut connection)
+            .unwrap();
+        let experiment_id_waiting: i32 = diesel::insert_into(crate::schema::experiment::table)
+            .values(&experiment_waiting)
+            .returning(crate::schema::experiment::id)
+            .get_result(&mut connection)
+            .unwrap();
+        let experiment_id_running: i32 = diesel::insert_into(crate::schema::experiment::table)
+            .values(&experiment_running)
+            .returning(crate::schema::experiment::id)
+            .get_result(&mut connection)
+            .unwrap();
+        let experiment_id_not_executed: i32 = diesel::insert_into(crate::schema::experiment::table)
+            .values(&experiment_not_executed)
+            .returning(crate::schema::experiment::id)
+            .get_result(&mut connection)
+            .unwrap();
+        let experiment_id_empty: i32 = diesel::insert_into(crate::schema::experiment::table)
+            .values(&experiment_empty)
+            .returning(crate::schema::experiment::id)
+            .get_result(&mut connection)
+            .unwrap();
+        let new_records_all: Vec<ExperimentExecution> = vec![
+            (ExecutionStatus::Aborted, experiment_id_all),
+            (ExecutionStatus::Failed, experiment_id_all),
+            (ExecutionStatus::Finished, experiment_id_all),
+            (ExecutionStatus::Running, experiment_id_all),
+            (ExecutionStatus::Finished, experiment_id_all),
+            (ExecutionStatus::Waiting, experiment_id_all),
+            (ExecutionStatus::Finished, experiment_id_waiting),
+            (ExecutionStatus::Waiting, experiment_id_waiting),
+            (ExecutionStatus::Aborted, experiment_id_waiting),
+            (ExecutionStatus::Finished, experiment_id_waiting),
+            (ExecutionStatus::Finished, experiment_id_running),
+            (ExecutionStatus::Running, experiment_id_running),
+            (ExecutionStatus::Aborted, experiment_id_running),
+            (ExecutionStatus::Finished, experiment_id_running),
+            (ExecutionStatus::Finished, experiment_id_not_executed),
+            (ExecutionStatus::Failed, experiment_id_not_executed),
+            (ExecutionStatus::Aborted, experiment_id_not_executed),
+            (ExecutionStatus::Finished, experiment_id_not_executed),
+        ]
+        .into_iter()
+        .enumerate()
+        .map(|(id, (status, experiment_id))| ExperimentExecution {
+            id: id as i32,
+            experiment_id,
+            pipeline_id: id.to_string(),
+            pipeline_step_id: id.to_string(),
+            execution_status: status.into(),
+            start_time: None,
+            end_time: None,
+            creation_time: chrono::Utc::now().naive_local(),
+        })
+        .collect();
+        diesel::insert_into(crate::schema::experiment_execution::table)
+            .values(&new_records_all)
+            .execute(&mut connection)
+            .unwrap();
+        assert!(ExperimentExecution::is_executed(experiment_id_all, &mut connection).unwrap());
+        assert!(ExperimentExecution::is_executed(experiment_id_waiting, &mut connection).unwrap());
+        assert!(ExperimentExecution::is_executed(experiment_id_running, &mut connection).unwrap());
+        assert!(
+            !ExperimentExecution::is_executed(experiment_id_not_executed, &mut connection).unwrap()
+        );
+        assert!(!ExperimentExecution::is_executed(experiment_id_empty, &mut connection).unwrap());
     }
 }
