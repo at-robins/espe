9d831e56a3b27c2329e79098a5f0350e3eb93528
at-robins <30956028+at-robins@users.noreply.github.com>
2025-11-11 08:41:17 +0100

    [35] Added ID hash to pipeline information
    The hashes of pipeline and step IDs have been added to the exchange format.

--- backend/src/model/internal/pipeline_blueprint.rs
+++ backend/src/model/internal/pipeline_blueprint.rs
@@ -3,7 +3,7 @@ use std::path::PathBuf;
 use getset::Getters;
 use serde::{Deserialize, Serialize};
 
-use crate::application::error::SeqError;
+use crate::application::{config::Configuration, error::SeqError};
 
 /// The pipeline sub-directory that imports are stored in.
 const IMPORT_DIRECOTRY: &str = "imports";
@@ -14,10 +14,14 @@ const IMPORT_END_TAG: &str = "</espe-import>";
 
 /// The definition of a pipeline step.
 #[derive(Debug, Clone, Getters, PartialEq, Serialize, Deserialize)]
+#[serde(from = "PipelineStepBlueprintSerde")]
 pub struct PipelineStepBlueprint {
-    /// The unique ID of the pipeline step.
+    /// The unique raw ID of the pipeline step.
     #[getset(get = "pub")]
     id: String,
+    /// The unique sanitised ID of the pipeline step which can safely be used as file name.
+    #[getset(get = "pub")]
+    sanitised_id: String,
     /// The name for display.
     #[getset(get = "pub")]
     name: String,
@@ -35,12 +39,48 @@ pub struct PipelineStepBlueprint {
     variables: Vec<PipelineStepVariable>,
 }
 
+/// The definition of a pipeline step.
+#[derive(Deserialize)]
+pub struct PipelineStepBlueprintSerde {
+    /// The unique ID of the pipeline step.
+    id: String,
+    /// The name for display.
+    name: String,
+    /// A description of the pipeline step.
+    description: String,
+    /// The container used to execute the pipeline step.
+    container: String,
+    /// The IDs of pipeline steps this step depends on.
+    dependencies: Vec<String>,
+    /// The variables that can be specified for the pipeline step.
+    variables: Vec<PipelineStepVariable>,
+}
+
+impl From<PipelineStepBlueprintSerde> for PipelineStepBlueprint {
+    fn from(value: PipelineStepBlueprintSerde) -> Self {
+        let hashed_id = Configuration::hash_string(&value.id);
+        Self {
+            id: value.id,
+            sanitised_id: hashed_id,
+            name: value.name,
+            description: value.description,
+            container: value.container,
+            dependencies: value.dependencies,
+            variables: value.variables,
+        }
+    }
+}
+
 /// The definition of a pipeline.
 #[derive(Debug, Clone, Getters, PartialEq, Serialize, Deserialize)]
+#[serde(from = "PipelineBlueprintSerde")]
 pub struct PipelineBlueprint {
-    /// The unique ID of the pipeline.
+    /// The unique raw ID of the pipeline as defined by the blueprint file.
     #[getset(get = "pub")]
     id: String,
+    /// The unique sanitised ID of the pipeline which can be safely used as file name.
+    #[getset(get = "pub")]
+    sanitised_id: String,
     /// The name for display.
     #[getset(get = "pub")]
     name: String,
@@ -56,6 +96,38 @@ pub struct PipelineBlueprint {
     /// The global variables that can be specified for the pipeline.
     #[getset(get = "pub")]
     global_variables: Vec<PipelineStepVariable>,
+}
+
+/// The definition of a pipeline without derived fields for deserialisation.
+#[derive(Deserialize)]
+struct PipelineBlueprintSerde {
+    /// The unique ID of the pipeline.
+    id: String,
+    /// The name for display.
+    name: String,
+    /// The version of the pipeline.
+    version: String,
+    /// A description of the pipeline.
+    description: String,
+    /// The [`PipelineStepBlueprint`] that make up the pipeline.
+    steps: Vec<PipelineStepBlueprint>,
+    /// The global variables that can be specified for the pipeline.
+    global_variables: Vec<PipelineStepVariable>,
+}
+
+impl From<PipelineBlueprintSerde> for PipelineBlueprint {
+    fn from(value: PipelineBlueprintSerde) -> Self {
+        let hashed_id = Configuration::hash_string(&value.id);
+        Self {
+            id: value.id,
+            sanitised_id: hashed_id,
+            name: value.name,
+            version: value.version,
+            description: value.description,
+            steps: value.steps,
+            global_variables: value.global_variables,
+        }
+    }
 }
 
 impl PipelineBlueprint {
@@ -276,14 +348,15 @@ mod tests {
             required: Some(false),
         };
         let global_variables = vec![test_var_1.clone(), test_var_2.clone(), test_var_3.clone()];
-        let blueprint = PipelineBlueprint {
+        let blueprint: PipelineBlueprint = PipelineBlueprintSerde {
             id: "Test ID".to_string(),
             name: "Test name".to_string(),
             version: "Test version".to_string(),
             description: "Test description".to_string(),
             steps,
             global_variables,
-        };
+        }
+        .into();
         let results = blueprint.global_variables_by_category(test_category);
         assert_eq!(results.len(), 2);
         assert!(results.contains(&&test_var_1));
@@ -314,23 +387,25 @@ mod tests {
             category: test_category.clone(),
             required: Some(false),
         };
-        let steps = vec![
-            PipelineStepBlueprint {
+        let steps: Vec<PipelineStepBlueprint> = vec![
+            PipelineStepBlueprintSerde {
                 id: "Step 1".to_string(),
                 name: "Step 1".to_string(),
                 description: "Step 1".to_string(),
                 container: "Step 1".to_string(),
                 dependencies: Vec::new(),
                 variables: vec![test_var_1.clone()],
-            },
-            PipelineStepBlueprint {
+            }
+            .into(),
+            PipelineStepBlueprintSerde {
                 id: "Step 2".to_string(),
                 name: "Step 2".to_string(),
                 description: "Step 2".to_string(),
                 container: "Step 2".to_string(),
                 dependencies: Vec::new(),
                 variables: vec![test_var_2.clone(), test_var_3.clone()],
-            },
+            }
+            .into(),
         ];
         let global_variables = vec![
             PipelineStepVariable {
@@ -355,14 +430,15 @@ mod tests {
                 required: Some(false),
             },
         ];
-        let blueprint = PipelineBlueprint {
+        let blueprint: PipelineBlueprint = PipelineBlueprintSerde {
             id: "Test ID".to_string(),
             name: "Test name".to_string(),
             version: "Test version".to_string(),
             description: "Test description".to_string(),
             steps,
             global_variables,
-        };
+        }
+        .into();
         let results = blueprint.step_variables_by_category(test_category);
         assert_eq!(results.len(), 2);
         assert!(results.contains(&&test_var_1));
