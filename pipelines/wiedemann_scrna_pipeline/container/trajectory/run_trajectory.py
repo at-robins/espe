ec182ee32b827e87fc8fc948facf74bdef64e821
Robin <robin.schenk@tum.de>
2025-12-05 14:31:22 +0100

    [22] Added more trajectory analysis methods
    PAGA and Monocle have been implemented.

--- pipelines/wiedemann_scrna_pipeline/container/trajectory/run_trajectory.py
+++ pipelines/wiedemann_scrna_pipeline/container/trajectory/run_trajectory.py
@@ -12,13 +12,22 @@ import scanpy as sc
 import seaborn as sns
 
 from matplotlib import pyplot as plt
+from py_monocle import (
+    learn_graph,
+    order_cells,
+    compute_cell_states,
+    regression_analysis,
+    differential_expression_genes,
+)
+from scipy import sparse
 
 MOUNT_PATHS = json.loads(os.environ.get("MOUNT_PATHS"))
 INPUT_FOLDER = next(iter(MOUNT_PATHS["dependencies"].values()))
 KEY_ROOT_CELL = "iroot"
 KEY_DIFFUSION_MAP_X = "X_diffmap"
 KEY_PSEUDOTIME_DIFFUSION = "dpt_pseudotime"
-KEY_PSEUDOTIME_DIFFUSION_SCALED = "dpt_pseudotime_scaled"
+KEY_PSEUDOTIME_MONOCLE = "monocle_pseudotime"
+KEY_CELLSTATE_MONOCLE = "monocle_cellstate"
 
 # Setup of scanpy.
 sc.settings.verbosity = 2
@@ -53,6 +62,13 @@ def cluster_name_to_cluster(obs_name):
     return obs_name.removeprefix("number_of_clusters_")
 
 
+def key_cluster_pseudotime(obs_name, pseudotime_key):
+    """
+    Returns a cluster specific version of the pseudotime, key.
+    """
+    return f"{pseudotime_key}_{cluster_name_to_cluster(obs_name)}"
+
+
 print("Loading cluster tree data...", flush=True)
 adata = anndata.read_h5ad(os.path.join(INPUT_FOLDER, "cluster_relation.h5ad"))
 print("Calculating diffusion maps...", flush=True)
@@ -60,13 +76,6 @@ sc.tl.diffmap(adata, n_comps=50)
 adata.uns[KEY_ROOT_CELL] = adata.obsm[KEY_DIFFUSION_MAP_X][:, 3].argmin()
 sc.tl.dpt(adata)
 print("Plotting data...", flush=True)
-# pdt_mean = np.mean(adata.obs[KEY_PSEUDOTIME_DIFFUSION])
-# pdt_std = np.std(adata.obs[KEY_PSEUDOTIME_DIFFUSION])
-# pdt_min = pdt_mean - 0.5 * pdt_std
-# pdt_max = pdt_mean + 0.5 * pdt_std
-# adata = adata[adata.obs[KEY_PSEUDOTIME_DIFFUSION] >= pdt_min]
-# adata = adata[adata.obs[KEY_PSEUDOTIME_DIFFUSION] <= pdt_max]
-# adata.obs[KEY_PSEUDOTIME_DIFFUSION_SCALED] = (adata.obs[KEY_PSEUDOTIME_DIFFUSION] - pdt_mean) / pdt_std
 fig = sc.pl.umap(
     adata,
     color=KEY_PSEUDOTIME_DIFFUSION,
@@ -74,9 +83,14 @@ fig = sc.pl.umap(
     show=False,
     return_fig=True,
 )
-fig.savefig(os.path.join(MOUNT_PATHS["output"], "pseudotime_umap.svg"))
+fig.savefig(os.path.join(MOUNT_PATHS["output"], "diffusion_pseudotime_umap.svg"))
 plt.close(fig)
 
+print("Setting up Monocle...", flush=True)
+monocle_umap_data = adata.obsm["X_umap"]
+monocle_expression_matrix = sparse.csr_matrix(adata.X)
+
+print("Analysing clusters...", flush=True)
 for obs_name in adata.obs:
     if is_cluster_obs(obs_name=obs_name):
         cluster = cluster_name_to_cluster(obs_name=obs_name)
@@ -84,7 +98,125 @@ for obs_name in adata.obs:
         if int(cluster) < 3:
             print(f"\t\tNot enough clusters. Skipping...", flush=True)
         else:
+            print("\t\tRunning Monocle...", flush=True)
+            key_monocle_pseudotime = key_cluster_pseudotime(
+                obs_name=obs_name, pseudotime_key=KEY_PSEUDOTIME_MONOCLE
+            )
+            key_monocle_cellstate = key_cluster_pseudotime(
+                obs_name=obs_name, pseudotime_key=KEY_CELLSTATE_MONOCLE
+            )
+            projected_points, mst, centroids = learn_graph(
+                matrix=monocle_umap_data, clusters=adata.obs[obs_name]
+            )
+            adata.obs[key_monocle_pseudotime] = order_cells(
+                monocle_umap_data,
+                centroids,
+                mst=mst,
+                projected_points=projected_points,
+                root_cells=adata.uns[KEY_ROOT_CELL],
+            )
+            cell_states, _ = compute_cell_states(monocle_umap_data, centroids, mst)
+            adata.obs[key_monocle_cellstate] = cell_states
+
+            print("\t\tPlotting Monocle...", flush=True)
+
+            fig_monocel_principal, ax_monocole_principal = plt.subplots(figsize=(8, 6))
+            sc.pl.umap(
+                adata,
+                color=obs_name,
+                legend_loc="on data",
+                show=False,
+                return_fig=False,
+                ax=ax_monocole_principal,
+            )
+            edges = np.array(mst.nonzero()).T
+            for edge in edges:
+                sns.lineplot(
+                    x=centroids[edge, 0],
+                    y=centroids[edge, 1],
+                    sort=False,
+                    color="black",
+                    linewidth=1,
+                    ax=ax_monocole_principal,
+                )
+            ax_monocole_principal.set_title("Principal graph")
+            fig_monocel_principal.savefig(
+                os.path.join(MOUNT_PATHS["output"], f"monocle_principal_{cluster}.svg")
+            )
+            plt.close(fig_monocel_principal)
+
+            fig_monocel_cellstate, ax_monocole_cellstate = plt.subplots(figsize=(8, 6))
+            sc.pl.umap(
+                adata,
+                color=key_monocle_cellstate,
+                legend_loc="on data",
+                show=False,
+                return_fig=False,
+                ax=ax_monocole_cellstate,
+            )
+            edges = np.array(mst.nonzero()).T
+            for edge in edges:
+                sns.lineplot(
+                    x=centroids[edge, 0],
+                    y=centroids[edge, 1],
+                    sort=False,
+                    color="black",
+                    linewidth=1,
+                    ax=ax_monocole_cellstate,
+                )
+            ax_monocole_cellstate.set_title("Principal graph with ceLtb4r1ll states")
+            fig_monocel_cellstate.savefig(
+                os.path.join(MOUNT_PATHS["output"], f"monocle_cellstate_{cluster}.svg")
+            )
+            plt.close(fig_monocel_cellstate)
+
+            fig_monocel_pseudotime = sc.pl.umap(
+                adata,
+                color=key_monocle_pseudotime,
+                legend_loc="on data",
+                show=False,
+                return_fig=True,
+            )
+            fig_monocel_pseudotime.savefig(
+                os.path.join(MOUNT_PATHS["output"], f"monocle_pseudotime_{cluster}.svg")
+            )
+            plt.close(fig_monocel_pseudotime)
+
+            print("\t\tPlotting Monocle differential gene expression...", flush=True)
+            morans_i_scores, pvalues, adjusted_pvalues = differential_expression_genes(
+                monocle_expression_matrix,
+                projected_cells=projected_points,
+            )
+            morani_results = pd.DataFrame(
+                {
+                    "Moran's I score": morans_i_scores,
+                    "p-value": pvalues,
+                    "Adjusted p-value": adjusted_pvalues,
+                },
+                index=pd.Series(adata.var_names, name="Gene"),
+            ).sort_values("Moran's I score", ascending=False)
+            morani_results.to_csv(
+                os.path.join(
+                    MOUNT_PATHS["output"], f"monocle_differential_gene_expression_{cluster}.csv"
+                ),
+                sep=",",
+                encoding="utf-8",
+            )
+            fig_monocel_dge = sc.pl.umap(
+                adata,
+                color=morani_results.index[:32],
+                legend_loc="on data",
+                show=False,
+                return_fig=True,
+            )
+            fig_monocel_dge.savefig(
+                os.path.join(MOUNT_PATHS["output"], f"monocle_dge_{cluster}.svg")
+            )
+            plt.close(fig_monocel_dge)
+
+            print("\t\tRunning PAGA...", flush=True)
             sc.tl.paga(adata, groups=obs_name)
+            print("\t\tPlotting PAGA...", flush=True)
             ax_paga = sc.pl.paga(adata, show=False)
             fig_paga_graph = ax_paga.get_figure()
             fig_paga_graph.savefig(
@@ -92,29 +224,13 @@ for obs_name in adata.obs:
             )
             plt.close(fig_paga_graph)
 
-            ax_paga = sc.pl.paga_compare(adata, show=False)
-            fig_paga_graph = ax_paga[0].get_figure()
-            fig_paga_graph.savefig(
-                os.path.join(MOUNT_PATHS["output"], f"paga_compare_{cluster}.svg")
-            )
-            plt.close(fig_paga_graph)
-            # sc.tl.umap(
-            #     adata,
-            #     init_pos="paga",
-            # )
-            # fig_paga_umap = sc.pl.umap(
-            #     adata,
-            #     color=[KEY_PSEUDOTIME_DIFFUSION, obs_name, "Gzma"],
-            #     legend_loc="on data",
-            #     show=False,
-            #     return_fig=True,
-            # )
-            # fig_paga_umap.savefig(
-            #     os.path.join(MOUNT_PATHS["output"], f"paga_umap_{cluster}.svg")
-            # )
-            # plt.close(fig_paga_umap)
-
-# print("\tWriting merged data to file...")
-# output_file_path = os.path.join(MOUNT_PATHS["output"], "merged.h5ad")
-# os.makedirs(MOUNT_PATHS["output"], exist_ok=True)
-# adata_merged.write(output_file_path, compression="gzip")
+            print("\t\tPlotting PAGA comparisons...", flush=True)
+            with plt.rc_context({"figure.figsize": (8, 8)}):
+                ax_paga = sc.pl.paga_compare(
+                    adata, show=False, threshold=0.10, edge_width_scale=0.25
+                )
+                fig_paga_graph = ax_paga[0].get_figure()
+                fig_paga_graph.savefig(
+                    os.path.join(MOUNT_PATHS["output"], f"paga_compare_{cluster}.svg")
+                )
+                plt.close(fig_paga_graph)
