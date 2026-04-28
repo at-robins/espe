321e84c66b76dcfca28b3cdd108144915fc64212
Robin <robin.schenk@tum.de>
2026-04-28 15:35:35 +0200

    [22] Improved feature selection
    The feature selection mechanism was improved.

--- pipelines/wiedemann_scrna_pipeline/container/feature_selection/run_feature_selection.py
+++ pipelines/wiedemann_scrna_pipeline/container/feature_selection/run_feature_selection.py
@@ -22,7 +22,8 @@ from scipy.sparse import issparse
 MOUNT_PATHS = json.loads(os.environ.get("MOUNT_PATHS"))
 INPUT_FOLDER = next(iter(MOUNT_PATHS["dependencies"].values()))
 TOP_FEATURES = 4000
-DEFAULT_BATCH_KEY = "replicate_name"
+REPLICATE_NAME_KEY = "replicate_name"
+SAMPLE_TYPE_KEY = "sample_type"
 
 # Setup of rpy2.
 rcb.logger.setLevel(logging.INFO)
@@ -56,8 +57,8 @@ def is_batch(adata: anndata.AnnData) -> bool:
     Returns true if the data contains batch information.
     """
     return (
-        DEFAULT_BATCH_KEY in adata.obs_keys()
-        and adata.obs[DEFAULT_BATCH_KEY].cat.categories.size > 1
+        REPLICATE_NAME_KEY in adata.obs_keys()
+        and adata.obs[REPLICATE_NAME_KEY].cat.categories.size > 1
     )
 
 
@@ -65,9 +66,9 @@ def calculate_deviance_unbatched(adata: anndata.AnnData):
     """
     Returns binomial deviance.
     """
-    print("\tLoading R dependencies...")
+    print("\t\tLoading R dependencies...")
     importr("scry")
-    print("\tRunning R code...")
+    print("\t\tRunning R code...")
     scry_function = ro.r(
         """
         function(data) {
@@ -78,57 +79,67 @@ def calculate_deviance_unbatched(adata: anndata.AnnData):
         }
         """
     )
-    return scry_function(adata).T
+    return np.nan_to_num(scry_function(adata).T)
 
 
-def calculate_deviance_batched(adata: anndata.AnnData):
+def get_deviance_mask_unbatched(adata: anndata.AnnData):
     """
-    Returns binomial deviance for batched data.
+    Returns the mask for the top deviant genes.
     """
-    batches = adata.obs[DEFAULT_BATCH_KEY].cat.categories
-    sum_squared_deviance = np.zeros(adata.n_vars)
-    for batch in batches:
-        print(f"\tCalculating deviance for batch {batch}...", flush=True)
-        adata_subset = adata[adata.obs[DEFAULT_BATCH_KEY] == batch]
-        # Calculate deviance and replace features not present in the
-        # subset with 0.
-        deviance = np.nan_to_num(calculate_deviance_unbatched(adata_subset))
-        # Sum up the square root of the batch deviances to negatively
-        # impact features not present in all batches.
-        sum_squared_deviance = sum_squared_deviance + np.sqrt(deviance)
-    return sum_squared_deviance
+    binomial_deviance = calculate_deviance_unbatched(adata)
+    sorted_indices = binomial_deviance.argsort()
+    if sorted_indices.size > TOP_FEATURES:
+        print(f"\t\t{sorted_indices.size} features present. Using top {TOP_FEATURES}.", flush=True)
+        sorted_indices = sorted_indices[-TOP_FEATURES:]
+    else:
+        print(f"\t\tOnly {sorted_indices.size} features present. Using all of them.", flush=True)
+    mask = np.zeros(adata.var_names.shape, dtype=bool)
+    mask[sorted_indices] = True
+    return mask
+
+
+def get_deviance_mask_batched(adata: anndata.AnnData):
+    """
+    Returns the mask for the top deviant genes for batched data.
+    """
+    sample_types = adata.obs[SAMPLE_TYPE_KEY].cat.categories
+    mask_total = np.zeros(adata.var_names.shape, dtype=bool)
+    for sample_type in sample_types:
+        print(f"\tCalculating deviance for batch {sample_type}...", flush=True)
+        mask_sample_type = None
+        sample_type_subset = adata[adata.obs[SAMPLE_TYPE_KEY] == sample_type]
+        replicates = sample_type_subset.obs[REPLICATE_NAME_KEY].cat.categories
+        for replicate in replicates:
+            print(f"\t\tCalculating deviance for replicate {replicate}...", flush=True)
+            replicate_subset = adata[adata.obs[SAMPLE_TYPE_KEY] == sample_type]
+            if mask_sample_type is None:
+                mask_sample_type = get_deviance_mask_unbatched(replicate_subset)
+            else:
+                mask_sample_type = np.logical_and(mask_total, get_deviance_mask_unbatched(replicate_subset))
+        mask_total = np.logical_or(mask_total, mask_sample_type)
+    return mask_total
 
 
 file_path_input = os.path.join(INPUT_FOLDER, "normalised.h5ad")
-print(f"Processing file {file_path_input}", flush=True)
-print("\tReading filtered data...")
+print(f"Processing file {file_path_input}")
+print("\tReading filtered data...", flush=True)
 adata = anndata.read_h5ad(file_path_input)
 adata.X = adata.layers["counts"]
 
 print("\tCalculating highly deviant genes...")
 if is_batch(adata):
     print("\tFound batch information.", flush=True)
-    batch_key = DEFAULT_BATCH_KEY
-    binomial_deviance = calculate_deviance_batched(adata)
+    batch_key = REPLICATE_NAME_KEY
+    adata.var["highly_deviant"] = get_deviance_mask_batched(adata)
 else:
     print("\tNo batch information found. Proceeding without...", flush=True)
     batch_key = None
-    binomial_deviance = calculate_deviance_unbatched(adata)
+    adata.var["highly_deviant"] = get_deviance_mask_unbatched(adata)
 
-sorted_indices = binomial_deviance.argsort()
-if sorted_indices.size > TOP_FEATURES:
-    print(f"\t{sorted_indices.size} features present. Using top {TOP_FEATURES}.")
-    sorted_indices = sorted_indices[-TOP_FEATURES:]
-else:
-    print(f"\tOnly {sorted_indices.size} features present. Using all of them.")
-mask = np.zeros(adata.var_names.shape, dtype=bool)
-mask[sorted_indices] = True
-adata.var["highly_deviant"] = mask
-adata.var["binomial_deviance"] = binomial_deviance
-print("\tCalculating highly variable genes...")
+print("\tCalculating highly variable genes...", flush=True)
 sc.pp.highly_variable_genes(adata, layer="scran_normalisation", batch_key=batch_key)
 
-print("\tPlotting data...")
+print("\tPlotting data...", flush=True)
 fig, ax = plt.subplots()
 sns.scatterplot(
     data=adata.var, x="means", y="dispersions", hue="highly_deviant", s=5, ax=ax
@@ -147,7 +158,7 @@ fig.tight_layout()
 os.makedirs(MOUNT_PATHS["output"], exist_ok=True)
 fig.savefig(os.path.join(MOUNT_PATHS["output"], "scatter_dispersion.svg"))
 
-print("\tWriting filtered data to file...")
+print("\tWriting filtered data to file...", flush=True)
 adata.write(
     os.path.join(MOUNT_PATHS["output"], "feature_selection.h5ad"),
     compression="gzip",
